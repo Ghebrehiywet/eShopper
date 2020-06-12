8a09c48f06280bf524450253fb9e6cd693e3c1f4
Ghebrehiywet Fissaha <g.hiwetmit@gmail.com>
2020-06-11 21:35:32 -0500

    init

---
+++ src/test/java/edu/miu/cs/cs425/eshopping/EshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs425.eshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
