cb37255233dff041633629a2bf20f5093cc40a2f
yohannesbaye <yohannesbaye@gmail.com>
2020-06-13 15:52:04 -0500

    merged

--- src/main/java/edu/miu/cs/cs425/eshopping/service/CategoryService.java
+++ src/main/java/edu/miu/cs/cs425/eshopping/service/CategoryService.java
@@ -2,10 +2,12 @@ package edu.miu.cs.cs425.eshopping.service;
 
 
 import edu.miu.cs.cs425.eshopping.model.Category;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface CategoryService {
+    public Page<Category> getAllCategoryPages(int pageNo, int size);
     public List<Category> getAllCategories();
     public Category createCategory(Category newCategory);
     public abstract Category findByIdCategory(Long id);
