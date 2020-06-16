a60ccd594810586923273636aa6a4e90084f0f52
Ghebrehiywet Fissaha <g.hiwetmit@gmail.com>
2020-06-16 08:11:56 -0500

    payment summary fix

--- src/main/java/edu/miu/cs/cs425/eshopping/repository/BuyerRepository.java
+++ src/main/java/edu/miu/cs/cs425/eshopping/repository/BuyerRepository.java
@@ -1,6 +1,7 @@
 package edu.miu.cs.cs425.eshopping.repository;
 
 import edu.miu.cs.cs425.eshopping.model.Buyer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
